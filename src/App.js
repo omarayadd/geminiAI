e6a268fd1f1a49df062f4b879550a76858da96f9
omar <omarrayad74@gmail.com>
2024-05-05 16:28:45 +0300

    finishing design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'; // Import useRef hook
+import { useState, useRef } from 'react';
 import './App.css';
 import runChat from './geminiAI';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,33 +7,42 @@ import { faQuestionCircle, faFileImage, faPaperPlane } from '@fortawesome/free-r
 
 
 function App() {
-  // const [answer, setAnswer] = useState('');
   const [pargraph, setParagraph] = useState([]);
   const [isExpanded, setIsExpanded] = useState(false);
   const [plus, setPlus] = useState("");
   const inputRef = useRef(null);
   const [history, setHistory] = useState([]);
   const [empty, setEmpty] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const Askme = async (inputValue) => {
     try {
+      setEmpty(false)
+      setIsLoading(true);
       const response = await runChat(inputValue);
       const paragraphs = response.text().split("**");
-      setParagraph(paragraphs)
-      setHistory([...history, inputValue])
+      setParagraph(paragraphs);
+      setHistory([...history, inputValue]);
     } catch (error) {
       console.error('Error:', error);
+    } finally {
+      setIsLoading(false); 
     }
   };
 
   const restoreAnswer = async(inputValue) =>{
     try {
+      setEmpty(false)
+      setIsLoading(true); 
       inputRef.current.value = inputValue;
       const response = await runChat(inputValue);
-      const paragraphs = response.text().split("**"); // Split response text into paragraphs
+      const paragraphs = response.text().split("**");
       setParagraph(paragraphs);
     } catch (error) {
       console.error('Error:', error);
+    }
+    finally {
+      setIsLoading(false); 
     }
   }
 
@@ -49,6 +58,7 @@ function App() {
 
   const reset = () =>{
     setParagraph([]);
+    setEmpty(true)
     inputRef.current.value = "";
   }
 
@@ -75,7 +85,7 @@ function App() {
       <div className={`${isExpanded ? 'first-half-open' : 'first-half-closed'}`}>
         <a href="#" onClick={expand}><FontAwesomeIcon icon={faBars} /></a>
         <div className={`${isExpanded ? 'plus-open' : 'plus-closed'}`}>
-            <a href="#" onClick={reset}><FontAwesomeIcon icon={faPlus} />  {plus}</a>
+            <a href="#" onClick={reset}><FontAwesomeIcon className="ayy"icon={faPlus} />  {plus}</a>
         </div>
         {isExpanded && <h3 className='hh3'>Recent</h3>} 
         <div className='items-div'>
@@ -88,19 +98,28 @@ function App() {
         </div>
         
         <div className='bottom-first-half'>
-          <div><a><FontAwesomeIcon icon={faQuestionCircle} /></a> {isExpanded && <span>Help</span>}</div>
+          <div><a><FontAwesomeIcon className="ayy" icon={faQuestionCircle} /></a> {isExpanded && <span>Help</span>}</div>
           <br></br>
-          <div><a><FontAwesomeIcon icon={faHistory} /></a> {isExpanded && <span>Activity</span>}</div>
+          <div><a><FontAwesomeIcon className="ayy" icon={faHistory} /></a> {isExpanded && <span>Activity</span>}</div>
           <br></br>
-          <div><a><FontAwesomeIcon icon={faCog} /></a> {isExpanded && <span>Settings</span>}</div>
+          <div><a><FontAwesomeIcon className="ayy" icon={faCog} /></a> {isExpanded && <span>Settings</span>}</div>
           <br></br>
         </div>
       </div>
       <div className="second-half">
+        {isLoading &&  (<div id="load">
+                  <div>G</div>
+                  <div>N</div>
+                  <div>I</div>
+                  <div>D</div>
+                  <div>A</div>
+                  <div>O</div>
+                  <div>L</div>
+                </div>)}
         <div className='upper-second-half'>
           <h2 className='title'>Gemini</h2>
           <div className='user-icon'></div>
-          {pargraph.length > 0 ? (
+          {pargraph.length > 0 && !isLoading ? (
             <div className='answer-box'>
               {pargraph.map((paragraph, index) => (
                 <p key={index}>{paragraph}</p>
@@ -109,7 +128,7 @@ function App() {
           ) : (
             <div className='tagroba'><h2 className='main-title'>How can I help you today?!</h2></div>
           )}
-          {pargraph.length === 0 && (
+          {pargraph.length === 0 && !isLoading &&  (
             <div className='ideas'>
               <div className='first-idea' onClick={() => handleIdeaClick('to see the pyramids in Egypt')}>
                 <h3>Plan a trip</h3>
@@ -136,7 +155,13 @@ function App() {
             <div className="icon-container">
               <a href='#'><FontAwesomeIcon icon={faFileImage} /></a>
               <a href='#'><FontAwesomeIcon icon={faMicrophone} /></a>
-              {!empty && <a href="#" onClick={() => Askme(inputRef.current.value)}><FontAwesomeIcon icon={faPaperPlane} /></a> }
+              {!empty && <a href="#" onClick={() => Askme(inputRef.current.value)}>
+              {isLoading ? ( 
+                <div className="spinner"></div>
+              ) : (
+                <FontAwesomeIcon icon={faPaperPlane} />
+              )}
+              </a> }
             </div>
           </div>
           <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
